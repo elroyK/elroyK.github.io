158856ffcb4b33beb8fb873b15498bf2ee3a691e
Christophe Leroy <Christophe Leroy>
2017-02-13 10:32:08 +0100

    Started modifying the landing page

--- js/sticky.js
+++
@@ -1,7 +0,0 @@
-   var $window = $(window),
-       $stickyEl = $('#sticky-nav'),
-       elTop = $stickyEl.offset().top;
-
-   $window.scroll(function() {
-        $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-    });
